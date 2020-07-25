fe43abd223349973a03b6823f0822c74a93c3bce
Arun Kumar <kumar05.962013@gmail.com>
2020-07-25 21:28:21 +0530

    Authform

--- screens/AuthScreens/SignInScreen.js
+++ screens/AuthScreens/SignInScreen.js
@@ -1,95 +1,25 @@
-import React, { useContext, useState } from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
-import { Input, Button } from "react-native-elements";
-import { NavigationEvents } from "react-navigation";
+import React, { useContext } from "react";
+import { View } from "react-native";
 
+import { NavigationEvents } from "react-navigation";
 import { Context as AuthContext } from "../../context/AuthContext";
-import { MaterialIcons, FontAwesome5 } from "@expo/vector-icons";
-import Colors from "../../constants/colors";
+import AuthForm from "../../components/AuthForm";
 
 const SignInScreen = ({ navigation }) => {
-  const [secureText, setSecureText] = useState(true);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
   const { state, signin, clearMessage } = useContext(AuthContext);
   const { errorMessage, successMessage } = state;
 
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1 }}>
       <NavigationEvents onWillFocus={clearMessage} />
-      <View style={styles.imgWrapper}>
-        <Image
-          source={require("../../assets/images/logo.png")}
-          style={styles.imgStyle}
-          resizeMode="contain"
-        />
-      </View>
-      <View style={styles.Form}>
-        <Input
-          inputStyle={styles.Input}
-          placeholder="Email Address"
-          value={email}
-          onChangeText={setEmail}
-          autoCapitalize="none"
-          autoCorrect={false}
-          autoFocus={true}
-          placeholderTextColor={Colors.placeholder}
-          leftIcon={
-            <MaterialIcons name="email" size={20} color={Colors.primary} />
-          }
-        />
-        <Input
-          secureTextEntry={secureText}
-          inputStyle={styles.Input}
-          placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
-          autoCapitalize="none"
-          autoCorrect={false}
-          placeholderTextColor={Colors.placeholder}
-          leftIcon={
-            <MaterialIcons name="lock" size={20} color={Colors.primary} />
-          }
-          rightIcon={
-            <FontAwesome5
-              name={secureText ? "eye-slash" : "eye"}
-              size={22}
-              color={Colors.primary}
-              onPress={() => setSecureText((prevState) => !prevState)}
-            />
-          }
-        />
-        {errorMessage ? (
-          <Text style={{ ...styles.message, ...styles.errorMessage }}>
-            {errorMessage}
-          </Text>
-        ) : null}
-        {successMessage ? (
-          <Text style={{ ...styles.message, ...styles.successMessage }}>
-            {successMessage}
-          </Text>
-        ) : null}
-        <TouchableOpacity onPress={() => console.log("forget")}>
-          <Text style={styles.forget}>Forget Password ?</Text>
-        </TouchableOpacity>
-        <View style={styles.btnWrapper}>
-          <Button
-            buttonStyle={styles.buttonStyle}
-            titleStyle={styles.btnTitle}
-            title="Sign in"
-            onPress={() => signin({ email, password })}
-          />
-        </View>
-        <TouchableOpacity
-          activeOpacity={1}
-          onPress={() => navigation.replace("Signup")}
-        >
-          <Text style={styles.newUser}>
-            New User ? <Text style={{ color: Colors.primary }}>Sign up</Text>
-          </Text>
-        </TouchableOpacity>
-      </View>
+      <AuthForm
+        isSignUpForm={false}
+        errorMessage={errorMessage}
+        successMessage={successMessage}
+        submitButtonText="Sign In"
+        navigation={navigation}
+        onSubmit={signin}
+      />
     </View>
   );
 };
@@ -99,66 +29,5 @@ SignInScreen.navigationOptions = () => {
     headerShown: false,
   };
 };
-
-const styles = StyleSheet.create({
-  screen: { flex: 1 },
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    fontFamily: "Roboto-regular",
-  },
-  imgWrapper: {
-    width: 150,
-    height: 150,
-    padding: 20,
-    overflow: "hidden",
-  },
-  imgStyle: {
-    width: "100%",
-    height: "100%",
-  },
-  Form: {
-    width: "80%",
-    marginVertical: 10,
-    marginTop: 25,
-  },
-  Input: {
-    fontSize: 16,
-    paddingLeft: 5,
-  },
-  forget: {
-    textAlign: "right",
-    color: Colors.primary,
-    fontFamily: "Roboto-bold",
-  },
-  btnWrapper: {
-    alignItems: "center",
-    marginVertical: 15,
-  },
-  buttonStyle: {
-    paddingVertical: 10,
-    paddingHorizontal: 50,
-    borderRadius: 30,
-    backgroundColor: Colors.primary,
-  },
-  btnTitle: {
-    color: Colors.white,
-    fontFamily: "Roboto-bold",
-  },
-  newUser: { textAlign: "center", fontFamily: "Roboto-bold" },
-  message: {
-    fontSize: 14,
-    textAlign: "center",
-    fontFamily: "Roboto-bold",
-    marginBottom: 5,
-  },
-  errorMessage: {
-    color: "red",
-  },
-  successMessage: {
-    color: "green",
-  },
-});
 
 export default SignInScreen;
--- screens/AuthScreens/SignUpScreen.js
+++ screens/AuthScreens/SignUpScreen.js
@@ -1,110 +1,25 @@
-import React, { useContext, useState } from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
-import { Input, Button } from "react-native-elements";
-import { NavigationEvents } from "react-navigation";
+import React, { useContext } from "react";
+import { View } from "react-native";
 
+import { NavigationEvents } from "react-navigation";
 import { Context as AuthContext } from "../../context/AuthContext";
-import { MaterialIcons, FontAwesome5 } from "@expo/vector-icons";
-import Colors from "../../constants/colors";
+import AuthForm from "../../components/AuthForm";
 
 const SignUpScreen = ({ navigation }) => {
-  const [secureText, setSecureText] = useState(true);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
   const { state, signup, clearMessage } = useContext(AuthContext);
   const { errorMessage, successMessage } = state;
 
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1 }}>
       <NavigationEvents onWillFocus={clearMessage} />
-      <View style={styles.imgWrapper}>
-        <Image
-          source={require("../../assets/images/logo.png")}
-          style={styles.imgStyle}
-          resizeMode="contain"
-        />
-      </View>
-      <View style={styles.Form}>
-        <Input
-          textContentType="name"
-          inputStyle={styles.Input}
-          value={name}
-          onChangeText={setName}
-          placeholder="Full Name"
-          autoCapitalize="none"
-          autoCorrect={false}
-          placeholderTextColor={Colors.placeholder}
-          leftIcon={
-            <FontAwesome5 name="user-alt" size={20} color={Colors.primary} />
-          }
-        />
-        <Input
-          textContentType="emailAddress"
-          inputStyle={styles.Input}
-          value={email}
-          onChangeText={setEmail}
-          placeholder="Email Address"
-          autoCapitalize="none"
-          autoCorrect={false}
-          placeholderTextColor={Colors.placeholder}
-          leftIcon={
-            <MaterialIcons name="email" size={20} color={Colors.primary} />
-          }
-        />
-        <Input
-          secureTextEntry={secureText}
-          textContentType="password"
-          inputStyle={styles.Input}
-          value={password}
-          onChangeText={setPassword}
-          placeholder="Password"
-          autoCapitalize="none"
-          autoCorrect={false}
-          placeholderTextColor={Colors.placeholder}
-          leftIcon={
-            <MaterialIcons name="lock" size={20} color={Colors.primary} />
-          }
-          rightIcon={
-            <FontAwesome5
-              name={secureText ? "eye-slash" : "eye"}
-              size={22}
-              color={Colors.primary}
-              onPress={() => setSecureText((prevState) => !prevState)}
-            />
-          }
-        />
-        {errorMessage ? (
-          <Text style={{ ...styles.message, ...styles.errorMessage }}>
-            {errorMessage}
-          </Text>
-        ) : null}
-        {successMessage ? (
-          <Text style={{ ...styles.message, ...styles.successMessage }}>
-            {successMessage}
-          </Text>
-        ) : null}
-        <View style={styles.btnWrapper}>
-          <Button
-            buttonStyle={styles.buttonStyle}
-            titleStyle={styles.btnTitle}
-            title="Sign up"
-            onPress={() => {
-              signup({ name, email, password });
-            }}
-          />
-        </View>
-        <TouchableOpacity
-          activeOpacity={1}
-          onPress={() => navigation.replace("Signin")}
-        >
-          <Text style={styles.newUser}>
-            Already Registered ?
-            <Text style={{ color: Colors.primary }}> Sign in</Text>
-          </Text>
-        </TouchableOpacity>
-      </View>
+      <AuthForm
+        isSignUpForm={true}
+        errorMessage={errorMessage}
+        successMessage={successMessage}
+        submitButtonText="Sign Up"
+        navigation={navigation}
+        onSubmit={signup}
+      />
     </View>
   );
 };
@@ -114,65 +29,5 @@ SignUpScreen.navigationOptions = () => {
     headerShown: false,
   };
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  imgWrapper: {
-    width: 150,
-    height: 150,
-    padding: 20,
-    overflow: "hidden",
-  },
-  imgStyle: {
-    width: "100%",
-    height: "100%",
-  },
-  Form: {
-    width: "80%",
-    marginVertical: 10,
-    marginTop: 25,
-  },
-  Input: {
-    fontSize: 16,
-    paddingLeft: 5,
-  },
-
-  forget: {
-    textAlign: "right",
-    color: Colors.primary,
-  },
-  btnWrapper: {
-    alignItems: "center",
-    marginVertical: 15,
-  },
-  buttonStyle: {
-    paddingVertical: 10,
-    paddingHorizontal: 50,
-    borderRadius: 30,
-    backgroundColor: Colors.primary,
-  },
-  btnTitle: {
-    color: Colors.white,
-  },
-  newUser: {
-    textAlign: "center",
-    fontFamily: "Roboto-bold",
-  },
-  message: {
-    fontSize: 14,
-    textAlign: "center",
-    fontFamily: "Roboto-bold",
-  },
-  errorMessage: {
-    color: "red",
-  },
-  successMessage: {
-    color: "green",
-  },
-});
 
 export default SignUpScreen;
