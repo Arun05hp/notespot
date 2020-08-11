2703ff0ea23f4722ba7f5a5c57b48abe6461c41f
Arun Kumar <kumar05.962013@gmail.com>
2020-08-11 12:59:41 +0530

    using yup validation for sing in

--- screens/AuthScreens/SignInScreen.js
+++ screens/AuthScreens/SignInScreen.js
@@ -1,12 +1,7 @@
 import React, { useContext, useState } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Keyboard,
-} from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Formik } from "formik";
+import * as Yup from "yup";
 
 import AppTextInput from "../../components/AppTextInput";
 import { Context as AuthContext } from "../../context/AuthContext";
@@ -16,72 +11,80 @@ import Colors from "../../constants/colors";
 import ErrorMsgBox from "../../components/ErrorMsgBox";
 
 import { MaterialIcons, FontAwesome5 } from "@expo/vector-icons";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
+
 const SignInScreen = ({ navigation }) => {
   const { state, signin, clearMessage } = useContext(AuthContext);
   const { errorMessage, successMessage, isLoading } = state;
 
   const [secureText, setSecureText] = useState(true);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
   return (
     <View style={styles.container}>
-      <View style={styles.Form}>
-        <AppLogo style={{ alignSelf: "center" }} />
-        <ErrorMsgBox
-          errorMessage={errorMessage}
-          successMessage={successMessage}
-        />
-
-        <AppTextInput
-          keyboardType="email-address"
-          value={email}
-          onChangeText={setEmail}
-          placeholder="Email"
-          placeholderTextColor={Colors.placeholder}
-          leftIcon={<MaterialIcons style={styles.iconStyle} name="email" />}
-        />
-        <AppTextInput
-          secureTextEntry={secureText}
-          value={password}
-          onChangeText={setPassword}
-          placeholder="Password"
-          placeholderTextColor={Colors.placeholder}
-          leftIcon={<MaterialIcons style={styles.iconStyle} name="lock" />}
-          rightIcon={
-            <FontAwesome5
-              name={secureText ? "eye-slash" : "eye"}
-              style={styles.iconStyle}
-              onPress={() => setSecureText((prevState) => !prevState)}
+      <AppLogo style={{ alignSelf: "center" }} />
+      <ErrorMsgBox
+        errorMessage={errorMessage}
+        successMessage={successMessage}
+      />
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={signin}
+        validationSchema={validationSchema}
+      >
+        {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
+          <>
+            <AppTextInput
+              errorMessage={touched.email ? errors.email : ""}
+              keyboardType="email-address"
+              leftIcon={<MaterialIcons style={styles.iconStyle} name="email" />}
+              onBlur={() => setFieldTouched("email")}
+              onChangeText={handleChange("email")}
+              placeholder="Email"
+              placeholderTextColor={Colors.placeholder}
             />
-          }
-        />
-
-        <TouchableOpacity onPress={() => console.log("forget")}>
-          <Text style={styles.forget}>Forget Password ?</Text>
-        </TouchableOpacity>
-
-        <CustomButton
-          style={styles.btnWrapper}
-          onPress={() => {
-            Keyboard.dismiss();
-            signin({ email, password });
-          }}
-          bgColor={Colors.primary}
-          color={Colors.white}
-          title="Sign In"
-          isLoading={isLoading}
-        />
-
-        <TouchableOpacity
-          activeOpacity={1}
-          onPress={() => navigation.replace("Signup")}
-        >
-          <Text style={styles.newUser}>
-            New User ? <Text style={{ color: Colors.primary }}>Sign Up</Text>
-          </Text>
-        </TouchableOpacity>
-      </View>
+            <AppTextInput
+              errorMessage={touched.password ? errors.password : ""}
+              leftIcon={<MaterialIcons style={styles.iconStyle} name="lock" />}
+              onBlur={() => setFieldTouched("password")}
+              onChangeText={handleChange("password")}
+              placeholder="Password"
+              placeholderTextColor={Colors.placeholder}
+              rightIcon={
+                <FontAwesome5
+                  name={secureText ? "eye-slash" : "eye"}
+                  style={styles.iconStyle}
+                  onPress={() => setSecureText((prevState) => !prevState)}
+                  secureTextEntry={secureText}
+                />
+              }
+            />
+            <TouchableOpacity onPress={() => console.log("forget")}>
+              <Text style={styles.forget}>Forget Password ?</Text>
+            </TouchableOpacity>
+            <CustomButton
+              style={styles.btnWrapper}
+              onPress={handleSubmit}
+              bgColor={Colors.primary}
+              color={Colors.white}
+              title="Sign In"
+              isLoading={isLoading}
+            />
+            <TouchableOpacity
+              activeOpacity={1}
+              onPress={() => navigation.replace("Signup")}
+            >
+              <Text style={styles.newUser}>
+                New User ?{" "}
+                <Text style={{ color: Colors.primary }}>Sign Up</Text>
+              </Text>
+            </TouchableOpacity>
+          </>
+        )}
+      </Formik>
     </View>
   );
 };
@@ -90,12 +93,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
-  },
-  Form: {
-    width: "80%",
+    paddingHorizontal: 20,
     marginVertical: 10,
   },
+
   iconStyle: {
     fontSize: 20,
     color: Colors.primary,
