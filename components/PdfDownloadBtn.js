2f59f1f2274056146d3f2de24067398e443e3b3b
Arun Kumar <kumar05.962013@gmail.com>
2020-08-15 08:16:27 +0530

    downloading pdf

--- components/PdfDownloadBtn.js
+++ components/PdfDownloadBtn.js
@@ -1,16 +1,77 @@
-import React from "react";
-import { TouchableOpacity, ActivityIndicator } from "react-native";
+import React, { useState } from "react";
+import { TouchableOpacity } from "react-native";
+import * as FileSystem from "expo-file-system";
 
+import baseUrl from "../api/baseUrl";
+import ProgressCircle from "react-native-progress-circle";
 import Colors from "../constants/colors";
 import { MaterialIcons } from "@expo/vector-icons";
 
 const PdfDownloadBtn = ({ item }) => {
+  const [state, setstate] = useState(false);
+  const [percentage, setPercentage] = useState(1);
+
+  const onSubmit = async ({ pdfName, pdfLink }) => {
+    const pdfFolder = FileSystem.documentDirectory + "pdfs/";
+    const folder = await FileSystem.getInfoAsync(pdfFolder);
+    if (!folder.exists) {
+      const res = await FileSystem.makeDirectoryAsync(pdfFolder);
+    }
+    const fileExists = await FileSystem.getInfoAsync(pdfFolder + pdfName);
+    if (fileExists.exists) {
+      console.log("presett", fileExists.exists);
+      setstate(false);
+      return;
+    }
+
+    const sourceUri = baseUrl + "/" + pdfLink;
+
+    const downloadResumable = FileSystem.createDownloadResumable(
+      sourceUri,
+      pdfFolder + pdfName,
+      {},
+      callback
+    );
+
+    try {
+      const { uri } = await downloadResumable.downloadAsync();
+      console.log("Finished downloading to ", uri);
+      setstate(false);
+    } catch (e) {
+      err = e;
+      setstate(false);
+    }
+  };
+  const callback = (downloadProgress) => {
+    const progress = Number.parseInt(
+      (
+        downloadProgress.totalBytesWritten /
+        downloadProgress.totalBytesExpectedToWrite
+      ).toFixed(3) * 100
+    );
+    console.log(progress);
+    setPercentage(progress);
+  };
   return (
-    <TouchableOpacity activeOpacity={0.5} onPress={() => console.log(item)}>
-      {false ? (
+    <TouchableOpacity
+      disabled={state}
+      activeOpacity={0.5}
+      onPress={() => {
+        setstate(true);
+        onSubmit(item);
+      }}
+    >
+      {!state ? (
         <MaterialIcons name="file-download" size={24} color={Colors.yellow} />
       ) : (
-        <ActivityIndicator size="small" color={Colors.yellow} />
+        <ProgressCircle
+          percent={percentage}
+          radius={10}
+          borderWidth={2}
+          color="#3399FF"
+          shadowColor="#999"
+          bgColor="#fff"
+        />
       )}
     </TouchableOpacity>
   );
--- screens/pdfScreen/PdfLists.js
+++ screens/pdfScreen/PdfLists.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { View, ActivityIndicator, Button } from "react-native";
-import * as FileSystem from "expo-file-system";
+
 import { Context as PdfContext } from "../../context/PdfContext";
 
 import defaultStyles from "../../constants/styles";
@@ -13,25 +13,10 @@ const PdfLists = ({ navigation }) => {
     getPdfs();
   }, []);
 
-  const onSubmit = async () => {
-    const pdfFolder = FileSystem.documentDirectory + "pdfs/";
-    const folder = await FileSystem.getInfoAsync(pdfFolder);
-
-    if (!folder.exists) {
-      const res = await FileSystem.makeDirectoryAsync(pdfFolder);
-      console.log(res);
-    }
-    const data = await FileSystem.readAsStringAsync(
-      pdfFolder + "Assignment.pdf",
-      { encoding: FileSystem.EncodingType.Base64 }
-    );
-  };
-
   if (true) {
     return (
       <View style={defaultStyles.flex_1_center}>
         <ActivityIndicator size="large" color="red" />
-        <Button title="ok" onPress={onSubmit} />
       </View>
     );
   }
